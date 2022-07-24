f94a0d55f8305695f9c70cbf73e6bf233b7e9673
Sagar Hapani <hapanisagar007@gmail.com>
2022-07-24 12:44:44 +0100

    my f commit

---
+++ github2.java
@@ -0,0 +1,12 @@
+package Gitpractice2;
+
+public class github2 {
+    public static void main(String[] args){
+        boolean a=true;
+        boolean b= false;
+        System.out.println("a="+a);
+        int A=20;
+        int B=10;
+        System.out.println("A+B="+(A+B));
+    }
+}
